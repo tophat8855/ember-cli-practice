12b77e8f7ba347682bbcaea9751720f115f0d2f3
tophat8855 <hrfarley@gmail.com>
2015-02-13 14:30:01 -0800

    if I could figure out the FixtureAdapter Stuff, it would work

--- app/models/experience.js
+++ app/models/experience.js
@@ -1,6 +1,19 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Experience = DS.Model.extend({
   description: DS.attr('string'),
   img: DS.attr('string'),
 });
+
+Experience.reopenClass({
+  FIXTURES: [
+    {id: 1, description: "Fussbudget", img: "assets/images/fuss.jpg"},
+    {id: 2, description: "Own Psychiatric Practice for 3 Years", img: "assets/images/psych.jpg"},
+    {id: 3, description: "Role of Christmas Queen in Christmas Play", img: "assets/images/queen.jpg"},
+    {id: 4, description: "Right Fielder for local baseball team", img: "assets/images/baseball.jpg"},
+    {id: 5, description: "Collected Halloween candy for kids trapped in delusions of the Great Pumpkin", img: "assets/images/halloween.jpg"},
+    {id: 6, description: "Assists local kids in football punting practice", img: "assets/images/football.jpg"},
+    ]
+});
+
+export default Experience;
--- app/models/interest.js
+++ app/models/interest.js
@@ -1,8 +1,18 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Interest = DS.Model.extend({
   description: DS.attr('string'),
   img: DS.attr('string'),
 });
 
-//where to fixtures go? with the CLI, how does ember-data work?
+Interest.reopenClass({
+  FIXTURES : [
+    {id: 1, description: "Classical Musicians", img: "assets/images/classical.jpg"},
+    {id: 2, description: "Warm Puppies", img: "assets/images/puppy.jpg"},
+    {id: 3, description: "Crashing busts of Beethoven", img: "assets/images/busts.gif"},
+    {id: 4, description: "Stomping out germs", img: "assets/images/germs.jpg"},
+    {id: 5, description: "Harassing baby brothers", img: "assets/images/rerun.jpg"},
+  ]
+});
+
+export default Interest;
